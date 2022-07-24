ef89170a1d1aa9398929480b468f27425a3bb851
mervebilic <merveebilic@icloud.com>
2022-07-24 19:02:00 +0300

    styles

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import resim1 from "../img/cosmos.jpg"
+
+function Header() {
+  return (
+    <div>
+
+        <div className='header'>
+        <img src={resim1}  className="logo"  />
+
+            COSMOS SHOES  
+            <div>
+              
+            </div>
+         </div>
+         
+
+    </div>
+  )
+}
+
+export default Header
